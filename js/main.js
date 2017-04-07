99a81b755d894c590f68e906cb542a88fd3be12d
4000D <dkdkajej@gmail.com>
2017-04-07 16:57:50 +0900

    team~

--- js/main.js
+++ js/main.js
@@ -107,34 +107,34 @@ $(document).ready(function() {
     paginationSpeed : 400,
     singleItem:true
   });
+});
 
-  /*====================================
-  Portfolio Isotope Filter
-  ======================================*/
-  $(window).load(function() {
-    var $container = $('#lightbox');
+/*====================================
+Portfolio Isotope Filter
+======================================*/
+$(window).load(function() {
+  var $container = $('#lightbox');
+  $container.isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 750,
+      easing: 'linear',
+      queue: false
+    }
+  });
+  $('.cat a').click(function() {
+    $('.cat .active').removeClass('active');
+    $(this).addClass('active');
+    var selector = $(this).attr('data-filter');
     $container.isotope({
-      filter: '*',
+      filter: selector,
       animationOptions: {
         duration: 750,
         easing: 'linear',
         queue: false
       }
     });
-    $('.cat a').click(function() {
-      $('.cat .active').removeClass('active');
-      $(this).addClass('active');
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false
-        }
-      });
-      return false;
-    });
-
+    return false;
   });
+
 });
