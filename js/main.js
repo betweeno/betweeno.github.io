a186a64e38d7dfaeb4560f4a9c05531dafed6bd1
4000D <dkdkajej@gmail.com>
2017-04-07 16:03:01 +0900

    team

--- js/main.js
+++ js/main.js
@@ -4,125 +4,137 @@
 //
 //
 
-function main() {
 
-(function () {
-   'use strict';
+function get_team_item (e) {
+  return ' <div class="item">' +
+  '<div class="thumbnail">' +
+  '<img src="' + e.image + '" alt="..." class="img-circle team-img"> ' +
+  '<div class="caption"> ' +
+  '<h3>' +e.name + '</h3> ' +
+  '<p>' + e.role + '</p> ' +
+  '<p>' + e.caption + '</p> ' +
+  '</div> ' +
+  '</div> ' +
+  '</div>'
+}
 
-   /* ==============================================
-  	Testimonial Slider
-  	=============================================== */
+$(document).ready(function() {
 
-  	$('a.page-scroll').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top - 40
-          }, 900);
-          return false;
-        }
+
+
+  /* ==============================================
+  Testimonial Slider
+  =============================================== */
+
+  $('a.page-scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 40
+        }, 900);
+        return false;
       }
-    });
+    }
+  });
 
-    /*====================================
-    Show Menu on Book
-    ======================================*/
-    $(window).bind('scroll', function() {
-      var navHeight = $(window).height() - 100;
-      if ($(window).scrollTop() > navHeight) {
-          $('.navbar-default').addClass('on');
-      } else {
-          $('.navbar-default').removeClass('on');
-      }
-    });
+  /*====================================
+  Show Menu on Book
+  ======================================*/
+  $(window).bind('scroll', function() {
+    var navHeight = $(window).height() - 100;
+    if ($(window).scrollTop() > navHeight) {
+      $('.navbar-default').addClass('on');
+    } else {
+      $('.navbar-default').removeClass('on');
+    }
+  });
 
-    $('body').scrollspy({
-      target: '.navbar-default',
-      offset: 80
-    })
+  $('body').scrollspy({
+    target: '.navbar-default',
+    offset: 80
+  })
 
-  	$(document).ready(function() {
-  	  $("#team").owlCarousel({
+  $.getJSON('data/teams.json')
+    .done(function(data) {
+      $("#team").html('');
 
-	      navigation : false, // Show next and prev buttons
-	      slideSpeed : 300,
-	      paginationSpeed : 400,
-	      autoHeight : true,
-	      itemsCustom : [
-			        [0, 1],
-			        [450, 2],
-			        [600, 2],
-			        [700, 2],
-			        [1000, 4],
-			        [1200, 4],
-			        [1400, 4],
-			        [1600, 4]
-			      ],
-  	  });
+      $.each(data, function(k, v) {
+        $("#team").append(get_team_item(v))
+      })
 
-  	  $("#clients").owlCarousel({
+      $("#team").owlCarousel({
 
-	      navigation : false, // Show next and prev buttons
-	      slideSpeed : 300,
-	      paginationSpeed : 400,
-	      autoHeight : true,
-	      itemsCustom : [
-			        [0, 1],
-			        [450, 2],
-			        [600, 2],
-			        [700, 2],
-			        [1000, 4],
-			        [1200, 5],
-			        [1400, 5],
-			        [1600, 5]
-			      ],
-  	  });
-
-      $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
-        });
+        autoHeight : true,
+        itemsCustom : [
+          [0, 1],
+          [450, 2],
+          [600, 2],
+          [700, 2],
+          [1000, 4],
+          [1200, 4],
+          [1400, 4],
+          [1600, 4]
+        ],
+      });
+    })
 
-  	});
+  $("#clients").owlCarousel({
 
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-      var $container = $('#lightbox');
+    navigation : false, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    autoHeight : true,
+    itemsCustom : [
+      [0, 1],
+      [450, 2],
+      [600, 2],
+      [700, 2],
+      [1000, 4],
+      [1200, 5],
+      [1400, 5],
+      [1600, 5]
+    ],
+  });
+
+  $("#testimonial").owlCarousel({
+    navigation : false, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true
+  });
+
+  /*====================================
+  Portfolio Isotope Filter
+  ======================================*/
+  $(window).load(function() {
+    var $container = $('#lightbox');
+    $container.isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
+    $('.cat a').click(function() {
+      $('.cat .active').removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
       $container.isotope({
-        filter: '*',
+        filter: selector,
         animationOptions: {
           duration: 750,
           easing: 'linear',
           queue: false
         }
       });
-      $('.cat a').click(function() {
-        $('.cat .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-          filter: selector,
-          animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-          }
-        });
-        return false;
-      });
-
+      return false;
     });
 
-
-
-}());
-
-
-}
-main();
+  });
+});
